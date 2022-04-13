1510b2bfd13002fb39d39a8d342f88c132f82955
Yeum Sang Kwon <kunshup2000@gmail.com>
2022-04-13 19:12:57 +0900

    feat: quiz06

---
+++ 06/quiz07/app.js
@@ -0,0 +1,44 @@
+const $ = (selector) => document.querySelector(selector);
+const getRandomInt = (max, min) => Math.floor(Math.random() * (max - min + 1)) + min;
+
+function App() {
+    const $score = $('#point');
+    const $life = $('#life');
+    const $box = $('#box');
+    const $bug = $('#bug');
+    const [MIN_X, MIN_Y, MAX_X, MAX_Y] = [0, 0, $box.offsetWidth - 20, $box.offsetHeight - 20];
+
+    this.moveBugPos = () => {
+        $bug.style.left = getRandomInt(MAX_X, MIN_X) + 'px';
+        $bug.style.top = getRandomInt(MAX_Y, MIN_Y) + 'px';
+    }
+
+    this.onBoxClick = () => {
+        $box.addEventListener('click', (e) => {
+            const { id } = e.target;
+
+            if (id === 'bug') {
+                $score.innerText = Number($score.innerText) + 1;
+            }
+
+            if (id === 'box') {
+                const curLife = Number($life.innerText) - 1;
+                $life.innerText = curLife;
+
+                if (!curLife) {
+                    alert('Game Over');
+
+                    $score.innerText = 0;
+                    $life.innerText = 10;
+                    return;
+                }
+            }
+                
+            this.moveBugPos();
+        });
+    }
+
+    this.onBoxClick();
+}
+
+new App();
