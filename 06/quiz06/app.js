a7a0ad99cd252aa54670639e0ae2b452786f49c5
Yeum Sang Kwon <kunshup2000@gmail.com>
2022-04-13 18:33:25 +0900

    feat: quiz6

--- 06/quiz06/app.js
+++ 06/quiz06/app.js
@@ -1,10 +1,65 @@
+const createDOMElement = (tagName, attrs) => {
+  const element = document.createElement(tagName);
+  attrs &&
+    attrs.forEach(({ attr, value }) => {
+      attr !== 'textContent'
+        ? element.setAttribute(attr, value)
+        : (element.textContent = value);
+    });
+  return element;
+};
 
 function App() {
     const $main = document.querySelector('main');
-    const $h1 = createDOM('h1', { 'text-content': 'Hello World' }, { 'class': 'title' });
+    const $h1 = createDOMElement('h1', [{ attr: 'textContent', value: '구구단' }]);
+    const $input = createDOMElement('input', [{ attr: 'type' }]);
+    const $button = createDOMElement('button', [{ attr: 'textContent', value: '구구단' }]);
+    const $ul = createDOMElement('ul');
 
-    console.log($h1);
+    this.init = () => {
+        [$h1, $input, $button, $ul].forEach($el => $main.appendChild($el));
+    }
+
+    this.multi = (number) => {
+        $ul.innerHTML = '';
+        
+        const $fragment = document.createDocumentFragment();
+
+        for (let i = 1; i <= 9; i++) {
+            $fragment.appendChild(createDOMElement('li', [{ attr: 'textContent', value: `${number} * ${i} = ${number * i}` }]));
+        }
+
+        $ul.appendChild($fragment);
+
+    }
+
+    this.registerEvent = () => { 
+        $input.addEventListener('keyup', (e) => {
+            if (e.keyCode === 13) {
+                const value = e.target.value;
+                $input.value = '';
+                if (isNaN(value)) {
+                    alert('숫자를 입력해주세요');
+                    return;
+                }
+                this.multi(value);
+                
+            }
+        });
+
+        $button.addEventListener('click', () => { 
+            const value = $input.value;
+            $input.value = '';
+            if (isNaN(value)) {
+                alert('숫자를 입력해주세요');
+                return;
+            }
+            this.multi(value);
+        });
+    }
+
+    this.registerEvent();
+    this.init();
 }
 
 new App();
-
