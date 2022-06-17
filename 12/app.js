b6c677c104e36bde2854c99b2a5a139a025ad8da
Yeum Sang Kwon <kunshup2000@gmail.com>
2022-06-17 11:53:43 +0900

    fix: module

--- 12/app.js
+++ 12/app.js
@@ -1,49 +1,47 @@
-import avg from "./avg.js";
-import rand from "./rand.js";
+import avg from './avg.js';
+import rand from './rand.js';
 
-const $avg = document.querySelector("#avg");
-const $rand = document.querySelector("#rand");
-const $input = document.querySelector("input[type=text]");
-const $minInput = document.querySelector("#min");
-const $maxInput = document.querySelector("#max");
+const $avg = document.querySelector('#avg');
+const $rand = document.querySelector('#rand');
+const $input = document.querySelector('input[type=text]');
+const $minInput = document.querySelector('#min');
+const $maxInput = document.querySelector('#max');
 
 const checkValidity = ({ value }) => {
-    const result = [];
-    try {
-        for (const v in value) {
-            const num = parseInt(value[v]);
-            if (isNaN(num)) continue;
-            result.push(num);
-        }
-        return result;
-    } catch (error) {
-        console.log(error);
+  const result = [];
+  try {
+    for (const v in value) {
+      const num = parseInt(value[v]);
+      if (isNaN(num)) continue;
+      result.push(num);
     }
-}
+    return result;
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-$avg.addEventListener("submit", e => {
-    e.preventDefault();
-    const value = $input.value
-        .replace(/\s/gi, '')
-        .split(",");
-    
-    const result = checkValidity({ value });
+$avg.addEventListener('submit', e => {
+  e.preventDefault();
+  const value = $input.value.replace(/\s/gi, '').split(',');
 
-    if (result) {
-        const avgResult = avg(...result);
-        alert(`평균값은 ${avgResult} 입니다.`);
-    }
+  const result = checkValidity({ value });
+
+  if (result) {
+    const avgResult = avg(...result);
+    alert(`평균값은 ${avgResult} 입니다.`);
+  }
 });
 
-$rand.addEventListener("submit", e => {
-    e.preventDefault();
-    const min = parseInt($minInput.value);
-    const max = parseInt($maxInput.value);
-   
-    const result = rand({
-        min: min || 0,
-        max
-    });
+$rand.addEventListener('submit', e => {
+  e.preventDefault();
+  const min = parseInt($minInput.value);
+  const max = parseInt($maxInput.value);
 
-    alert(`추출된 값은 ${result} 입니다.`);
+  const result = rand({
+    min: min || 0,
+    max,
+  });
+
+  alert(`추출된 값은 ${result} 입니다.`);
 });
