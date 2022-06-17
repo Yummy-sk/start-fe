2f504c5f1b327924a63eaddefd059274721a5dd0
Yeum Sang Kwon <kunshup2000@gmail.com>
2022-06-17 12:07:58 +0900

    fix: fetch 예제 수정

--- 09/app.js
+++ 09/app.js
@@ -1,58 +1,55 @@
 const _fetch = async ({ url }) => {
-    try {
-        const response = await fetch(url, {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json',
-                Accept: 'application/json',
-            },
-        });
+  try {
+    const response = await fetch(url, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+      },
+    });
 
-        if (!response.ok) {
-            throw new Error('API Call Fail');
-        }
-
-        return data;
-    } catch (e) {
-        console.error(e.message);
-        return e.message
+    if (!response.ok) {
+      throw new Error('API Call Fail');
     }
-}
 
+    return await response.text();
+  } catch (e) {
+    console.error(e.message);
+    return e.message;
+  }
+};
 
 function App() {
-    const $input = document.querySelector('#url');
-    const $button = document.querySelector('button');
-    const $textArea = document.querySelector('#log');
+  const $input = document.querySelector('#url');
+  const $button = document.querySelector('button');
+  const $textArea = document.querySelector('#log');
 
-    this.state = {
-        input: '',
-        content: '',
-    }
+  this.state = {
+    input: '',
+    content: '',
+  };
 
-    this.handleInput = () => {
-        $input.addEventListener('input', (e) => {
-            this.state = { ...this.state, input: e.target.value };
-        })
-    }
+  this.handleInput = () => {
+    $input.addEventListener('input', e => {
+      this.state = { ...this.state, input: e.target.value };
+    });
+  };
 
-    this.handleButtonClick = () => {
-        $button.addEventListener('click', async () => {
-            
-            const response = await _fetch({ url: this.state.input });
+  this.handleButtonClick = () => {
+    $button.addEventListener('click', async () => {
+      const response = await _fetch({ url: this.state.input });
 
-            this.state = { ...this.state, content: response };
+      this.state = { ...this.state, content: response };
 
-            $textArea.value = this.state.content;
-        });
-    }
+      $textArea.value = this.state.content;
+    });
+  };
 
-    this.init = () => {
-        this.handleInput();
-        this.handleButtonClick();
-    }
+  this.init = () => {
+    this.handleInput();
+    this.handleButtonClick();
+  };
 }
 
 const app = new App();
 app.init();
-
